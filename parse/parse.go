3c9fc0fe10b51912b99f10c572ab62596b4397a1
Yuya Isaka <yuyaisaka@rem.local>
2022-02-05 16:31:52 +0900

    fix

---
+++ parse/parse.go
@@ -0,0 +1,169 @@
+package parse
+
+import (
+	h "github.com/yuya-isaka/ccgo/header"
+)
+
+func equalStr(s string) bool {
+	return h.Tok[h.TokNum].Str == s
+}
+
+func equalKind(tk h.TokenKind) bool {
+	return h.Tok[h.TokNum].Kind == tk
+}
+
+func equalStrGo(s string) bool {
+	if h.Tok[h.TokNum].Str == s {
+		h.GoTok(1)
+		return true
+	} else {
+		return false
+	}
+}
+
+func hopeStrGo(s string) {
+	if !equalStr(s) {
+		h.ErrorToken(s)
+	}
+	defer h.GoTok(1)
+}
+
+func newNode(kind h.NodeKind) *h.Node {
+	var node *h.Node = &h.Node{}
+	node.Kind = kind
+	return node
+}
+
+func newNum(val int) *h.Node {
+	var node *h.Node = newNode(h.ND_NUM)
+	node.Val = val
+	return node
+}
+
+func newBinary(kind h.NodeKind, lhs *h.Node, rhs *h.Node) *h.Node {
+	var node *h.Node = newNode(kind)
+	node.Lhs = lhs
+	node.Rhs = rhs
+	return node
+}
+
+func newUnary(kind h.NodeKind, expr *h.Node) *h.Node {
+	var node *h.Node = newNode(kind)
+	node.Lhs = expr
+	return node
+}
+
+func Expr() *h.Node {
+	return equality()
+}
+
+func equality() *h.Node {
+	var node *h.Node = relational()
+
+	for {
+		if equalStrGo("==") {
+			node = newBinary(h.ND_EQ, node, relational())
+			continue
+		}
+
+		if equalStrGo("!=") {
+			node = newBinary(h.ND_NE, node, relational())
+			continue
+		}
+
+		return node
+	}
+}
+
+func relational() *h.Node {
+	var node *h.Node = add()
+
+	for {
+		if equalStrGo("<") {
+			node = newBinary(h.ND_LT, node, add())
+			continue
+		}
+
+		if equalStrGo("<=") {
+			node = newBinary(h.ND_LE, node, add())
+			continue
+		}
+
+		if equalStrGo(">") {
+			node = newBinary(h.ND_LT, add(), node)
+			continue
+		}
+
+		if equalStrGo(">=") {
+			node = newBinary(h.ND_LE, add(), node)
+			continue
+		}
+
+		return node
+	}
+}
+
+func add() *h.Node {
+	var node *h.Node = mul()
+
+	for {
+		if equalStrGo("+") {
+			node = newBinary(h.ND_ADD, node, mul())
+			continue
+		}
+
+		if equalStrGo("-") {
+			node = newBinary(h.ND_SUB, node, mul())
+			continue
+		}
+
+		return node
+	}
+}
+
+func mul() *h.Node {
+	var node *h.Node = unary()
+
+	for {
+		if equalStrGo("*") {
+			node = newBinary(h.ND_MUL, node, unary())
+			continue
+		}
+
+		if equalStrGo("/") {
+			node = newBinary(h.ND_DIV, node, unary())
+			continue
+		}
+
+		return node
+	}
+}
+
+func unary() *h.Node {
+	if equalStrGo("+") {
+		return unary()
+	}
+
+	if equalStrGo("-") {
+		return newUnary(h.ND_NEG, unary())
+	}
+
+	return primary()
+}
+
+func primary() *h.Node {
+	if equalStrGo("(") {
+		var node *h.Node = Expr()
+		hopeStrGo(")")
+		return node
+	}
+
+	if equalKind(h.TK_NUM) {
+		var node *h.Node = newNum(h.Tok[h.TokNum].Val)
+		defer h.GoTok(1)
+		return node
+	}
+
+	h.ErrorToken("expected an expression")
+	return newNode(h.ND_NUM) // ここは実行されないはず
+}
---
+++ tokenize/tokenize.go
@@ -0,0 +1,108 @@
+package tokenize
+
+import (
+	"fmt"
+	"strconv"
+
+	h "github.com/yuya-isaka/ccgo/header"
+)
+
+func newToken(kind h.TokenKind) *h.Token {
+	var tok *h.Token = &h.Token{} // new(Token)と同じ
+	tok.Kind = kind
+	tok.Loc = h.TextNum
+	return tok
+}
+
+func goText(i int) {
+	h.TextNum += i
+}
+
+func checkNum() int {
+	var result int = 0
+	for _, t := range h.Text[h.TextNum:] {
+		if v, err := strconv.Atoi(t); err == nil {
+			result = result*10 + v
+			goText(1)
+		} else {
+			break
+		}
+	}
+	return result
+}
+
+func errorText(s string) {
+	fmt.Println()
+	for i, v := range h.Text {
+		fmt.Printf("%dth: %s\n", i, v)
+	}
+	fmt.Println()
+	panic(fmt.Sprintf("%dth text, %s\n", h.TextNum, s))
+}
+
+func ispunctLast(s1 string) int {
+	if s1 == "+" || s1 == "-" || s1 == "*" || s1 == "/" || s1 == "(" || s1 == ")" || s1 == "<" || s1 == ">" || s1 == "=" {
+		return 1
+	} else {
+		return 0
+	}
+}
+
+func ispunct(s1, s2 string) int {
+	if (s1+s2) == "==" || (s1+s2) == "!=" || (s1+s2) == "<=" || (s1+s2) == ">=" {
+		return 2
+	} else {
+		return ispunctLast(s1)
+	}
+}
+
+func Tokenize() []*h.Token {
+	var result []*h.Token = make([]*h.Token, 0)
+
+	for len(h.Text) > h.TextNum {
+		if h.Text[h.TextNum] == " " {
+			goText(1)
+			continue
+		}
+
+		if _, err := strconv.Atoi(h.Text[h.TextNum]); err == nil {
+			var cur *h.Token = newToken(h.TK_NUM)
+			var tmp int = checkNum()
+			cur.Val = tmp
+			cur.Str = strconv.Itoa(tmp)
+			result = append(result, cur)
+			h.GoTok(1)
+			continue
+		}
+
+		var flag int = 0
+		if len(h.Text)-1 > h.TextNum {
+			flag = ispunct(h.Text[h.TextNum], h.Text[h.TextNum+1])
+		} else {
+			flag = ispunctLast(h.Text[h.TextNum])
+		}
+		if flag == 1 || flag == 2 {
+			var cur *h.Token = newToken(h.TK_PUNCT)
+			cur.Str = h.Text[h.TextNum]
+			if flag == 2 {
+				cur.Str += h.Text[h.TextNum+1]
+			}
+			goText(flag)
+			result = append(result, cur)
+			h.GoTok(1)
+			continue
+		}
+
+		errorText("invalid text")
+	}
+
+	var cur *h.Token = newToken(h.TK_EOF)
+	cur.Str = "EOF"
+	result = append(result, cur)
+	h.GoTok(1)
+
+	h.TokSum = h.TokNum
+	h.TokNum = 0
+
+	return result
+}
